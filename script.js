d28ffe60980b435ff96ba723ec42209c547047cd
CristianTobar07 <cristianndres@unicauca.edu.co>
2024-12-13 14:58:23 -0500

    feat: update script portafolio and update images to webp format

--- script.js
+++ script.js
@@ -15,31 +15,94 @@ function seleccionar() {
   document.getElementById("nav").classList = "";
   menuVisible = false;
 }
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades() {
+
+const listTechnisSkills = [
+  { key: "javascript", name: "Javascript", percentage: 95 },
+  { key: "typescript", name: "Typescript", percentage: 95 },
+  { key: "react", name: "React", percentage: 95 },
+  { key: "reactnativo", name: "React Native", percentage: 85 },
+  { key: "angular", name: "Angular", percentage: 95 },
+  { key: "vue", name: "Vue", percentage: 85 },
+  { key: "nodejs", name: "Node.js", percentage: 90 },
+  { key: "database", name: "Bases de datos", percentage: 90 },
+];
+
+const listProfesionalSkills = [
+  { key: "comunicacion", name: "Comunicación", percentage: 95 },
+  { key: "trabajo", name: "Trabajo", percentage: 95 },
+  { key: "creatividad", name: "Creatividad", percentage: 100 },
+  { key: "dedicacion", name: "Dedicación", percentage: 100 },
+  { key: "proyect", name: "Proyección", percentage: 95 },
+];
+
+function createElements(idElement, listSkills) {
+  const container = document.getElementById(idElement);
+
+  listSkills.forEach((skill) => {
+    const skillDiv = document.createElement("div");
+    skillDiv.classList.add("skill");
+    const skillName = document.createElement("span");
+    skillName.textContent = skill.name;
+    const barraSkill = document.createElement("div");
+    barraSkill.classList.add("barra-skill");
+    const progreso = document.createElement("div");
+    progreso.classList.add("progreso");
+    progreso.style.width = `${skill.percentage}%`;
+    const porcentaje = document.createElement("span");
+    porcentaje.textContent = `${skill.percentage}%`;
+
+    progreso.appendChild(porcentaje);
+    barraSkill.appendChild(progreso);
+    skillDiv.appendChild(skillName);
+    skillDiv.appendChild(barraSkill);
+    container.appendChild(skillDiv);
+  });
+}
+
+function effeectSkills(listSkills) {
   const skills = document.getElementById("skills");
   const distancia_skills =
     window.innerHeight - skills.getBoundingClientRect().top;
-  console.log({ distancia_skills });
   if (distancia_skills >= 300) {
     let habilidades = document.getElementsByClassName("progreso");
-    habilidades[0].classList.add("javascript");
-    habilidades[1].classList.add("typescript");
-    habilidades[2].classList.add("react");
-    habilidades[3].classList.add("reactnativo");
-    habilidades[4].classList.add("angular");
-    habilidades[5].classList.add("vue");
-    habilidades[6].classList.add("nodejs");
-    habilidades[7].classList.add("nosql");
-    habilidades[8].classList.add("comunicacion");
-    habilidades[9].classList.add("trabajo");
-    habilidades[10].classList.add("creatividad");
-    habilidades[11].classList.add("dedicacion");
-    habilidades[12].classList.add("proyect");
+    listSkills.forEach((skill, index) => {
+      return habilidades[index].classList.add(skill.key);
+    });
   }
 }
 
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
+function addPercentageSkills(listSkills) {
+  listSkills.forEach((skill) => {
+    const styles = `
+      .skills .skill .${skill.key} {
+        width: 0%;
+        animation: 2s ${skill.key} forwards;
+      }
+      @keyframes ${skill.key} {
+        0% {
+          width: 0%;
+        }
+        100% {
+          width: ${skill.percentage}%;
+        }
+      }
+    `;
+
+    const styleSheet = document.createElement("style");
+    styleSheet.nodeType = "text/css";
+    styleSheet.textContent = styles;
+    document.head.appendChild(styleSheet);
+
+    return true;
+  });
+}
+
+createElements("col_left", listTechnisSkills);
+createElements("col_right", listProfesionalSkills);
+addPercentageSkills(listTechnisSkills);
+addPercentageSkills(listProfesionalSkills);
+
 window.onscroll = function () {
-  efectoHabilidades();
+  effeectSkills(listTechnisSkills);
+  // effeectSkills(listProfesionalSkills);
 };
